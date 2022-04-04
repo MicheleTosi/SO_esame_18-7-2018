b96ebe2e795e9eca9086e01482f57a37f8772c57
Michele Tosi <micheletosi98@gmail.com>
2022-04-04 23:15:27 +0200

    Update 8_21-1-2020.c

--- Esami/8_21-1-2020.c
+++ Esami/8_21-1-2020.c
@@ -1,3 +1,27 @@
+/**
+SPECIFICATION TO BE IMPLEMENTED:
+Implementare una programma che ricevento in input tramite argv[] una stringa S
+esegua le seguenti attivita'.
+Il main thread dovra' attivare due nuovi thread, che indichiamo con T1 e T2.
+Successivamente il main thread dovra' leggere indefinitamente caratteri dallo 
+standard input, a blocchi di 5 per volta, e dovra' rendere disponibili i byte 
+letti a T1 e T2. 
+Il thread T1 dovra' inserire di volta in volta i byte ricevuti dal main thread 
+in coda ad un file di nome S_diretto, che dovra' essere creato. 
+Il thread T2 dovra' inserirli invece nel file S_inverso, che dovra' anche esso 
+essere creato, scrivendoli ogni volta come byte iniziali del file (ovvero in testa al 
+file secondo uno schema a pila).
+
+L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso
+WinAPI) in modo tale che quando il processo venga colpito esso dovra' 
+calcolare il numero dei byte che nei due file hanno la stessa posizione ma sono
+tra loro diversi in termini di valore. Questa attivita' dovra' essere svolta attivando 
+per ogni ricezione di segnale un apposito thread.
+
+In caso non vi sia immissione di dati sullo standard input, l'applicazione dovra' 
+utilizzare non piu' del 5% della capacita' di lavoro della CPU.
+**/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
