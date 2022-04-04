33314db89f710cc264f4c8df18931dea77db3e5e
Michele Tosi <micheletosi98@gmail.com>
2022-04-04 23:12:08 +0200

    Update 5_23-1-2019.c

--- Esami/5_23-1-2019.c
+++ Esami/5_23-1-2019.c
@@ -1,3 +1,33 @@
+/**
+SPECIFICATION TO BE IMPLEMENTED:
+Implementare una programma che riceva in input, tramite argv[], il nome
+di un file F ed N stringhe S_1 .. S_N (con N maggiore o uguale
+ad 1.
+Per ogni stringa S_i dovra' essere attivato un nuovo thread T_i, che fungera'
+da gestore della stringa S_i.
+Il main thread dovra' leggere indefinitamente stringhe dallo standard-input. 
+Ogni nuova stringa letta dovra' essere comunicata a tutti i thread T_1 .. T_N
+tramite un buffer condiviso, e ciascun thread T_i dovra' verificare se tale
+stringa sia uguale alla stringa S_i da lui gestita. In caso positivo, ogni
+carattere della stringa immessa dovra' essere sostituito dal carattere '*'.
+Dopo che i thread T_1 .. T_N hanno analizzato la stringa, ed eventualmente questa
+sia stata modificata, il main thread dovra' scrivere tale stringa (modificata o non)
+su una nuova linea del file F. 
+In altre parole, la sequenza di stringhe provenienti dallo standard-input dovra' 
+essere riportata su file F in una forma 'epurata'  delle stringhe S1 .. SN, 
+che verranno sostituite da strighe  della stessa lunghezza costituite esclusivamente
+da sequenze del carattere '*'.
+Inoltre, qualora gia' esistente, il file F dovra' essere troncato (o rigenerato) 
+all'atto del lancio dell'applicazione.
+
+L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso
+WinAPI) in modo tale che quando il processo venga colpito esso dovra' 
+riversare su standard-output il contenuto corrente del file F.
+
+Qualora non vi sia immissione di input, l'applicazione dovra' utilizzare 
+non piu' del 5% della capacita' di lavoro della CPU.
+**/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
