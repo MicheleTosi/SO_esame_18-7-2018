12e2d764ca18918161cb9a0ede4483105d3bd7c2
Michele Tosi <micheletosi98@gmail.com>
2022-04-04 23:13:44 +0200

    Update 6_18-9-2019.c

--- Esami/6_18-9-2019.c
+++ Esami/6_18-9-2019.c
@@ -1,3 +1,31 @@
+/**
+SPECIFICATION TO BE IMPLEMENTED:
+Implementare una programma che riceva in input, tramite argv[], un insieme di
+stringhe S_1 ..... S_n con n maggiore o uguale ad 1. 
+Per ogni stringa S_i dovra' essere attivato un thread T_i.
+Il main thread dovra' leggere indefinitamente stringhe dallo standard-input.
+Ogni stringa letta dovra' essere resa disponibile al thread T_1 che dovra' 
+eliminare dalla stringa ogni carattere presente in S_1, sostituendolo con il 
+carattere 'spazio'.
+Successivamente T_1 rendera' la stringa modificata disponibile a T_2 che dovra' 
+eseguire la stessa operazione considerando i caratteri in S_2, e poi la passera' 
+a T_3 (che fara' la stessa operazione considerando i caratteri in S_3) e cosi' 
+via fino a T_n. 
+T_n, una volta completata la sua operazione sulla stringa ricevuta da T_n-1, dovra'
+passare la stringa ad un ulteriore thread che chiameremo OUTPUT il quale dovra' 
+stampare la stringa ricevuta su un file di output dal nome output.txt.
+Si noti che i thread lavorano secondo uno schema pipeline, sono ammesse quindi 
+operazioni concorrenti su differenti stringhe lette dal main thread dallo 
+standard-input.
+
+L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso
+WinAPI) in modo tale che quando il processo venga colpito esso dovra' 
+stampare il contenuto corrente del file output.txt su standard-output.
+
+In caso non vi sia immissione di dati sullo standard-input, l'applicazione 
+dovra' utilizzare non piu' del 5% della capacita' di lavoro della CPU.
+**/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<string.h>
