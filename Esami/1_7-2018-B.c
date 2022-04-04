5c941daab3a1ea965ee436ad27ef81c9f52c134f
Michele Tosi <micheletosi98@gmail.com>
2022-04-04 23:02:56 +0200

    Update 1_7-2018-B.c

--- Esami/1_7-2018-B.c
+++ Esami/1_7-2018-B.c
@@ -2,20 +2,15 @@
 SPECIFICATION TO BE IMPLEMENTED:
 Implementare un programma che riceva in input tramite argv[] i pathname 
 associati ad N file, con N maggiore o uguale ad 1. Per ognuno di questi
-file generi un processo che legga tutte le stringhe contenute in quel file
-e le scriva in un'area di memoria condivisa con il processo padre. Si 
-supponga per semplicita' che lo spazio necessario a memorizzare le stringhe
-di ognuno di tali file non ecceda 4KB. 
-Il processo padre dovra' attendere che tutti i figli abbiano scritto in 
-memoria il file a loro associato, e successivamente dovra' entrare in pausa
-indefinita.
-D'altro canto, ogni figlio dopo aver scritto il contenuto del file nell'area 
-di memoria condivisa con il padre entrera' in pausa indefinita.
-L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso
-WinAPI) in modo tale che quando il processo padre venga colpito da esso dovra' 
-stampare a terminale il contenuto corrente di tutte le aree di memoria 
-condivisa anche se queste non sono state completamente popolate dai processi 
-figli. **/
+file generi un thread (quindi in totale saranno generati N nuovi thread 
+concorrenti). 
+Successivamente il main-thread acquisira' stringhe da standard input in 
+un ciclo indefinito, ed ognuno degli N thread figli dovra' scrivere ogni
+stringa acquisita dal main-thread nel file ad esso associato.
+L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso 
+WinAPI) in modo tale che quando uno qualsiasi dei thread dell'applicazione
+venga colpito da esso dovra' stampare a terminale tutte le stringhe gia' 
+immesse da standard-input e memorizzate nei file destinazione. **/
 
 
 #include<stdio.h>
