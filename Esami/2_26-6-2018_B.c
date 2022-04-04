66dc392f18a9579f598abe47ba804688f52770c9
Michele Tosi <micheletosi98@gmail.com>
2022-04-04 23:07:43 +0200

    Update 2_26-6-2018_B.c

--- Esami/2_26-6-2018_B.c
+++ Esami/2_26-6-2018_B.c
@@ -1,3 +1,21 @@
+/**
+SPECIFICATION TO BE IMPLEMENTED:
+Implementare un programma che riceva in input tramite argv[2] un numero
+intero N maggiore o uguale ad 1 (espresso come una stringa di cifre 
+decimali), e generi N nuovi processi. Ciascuno di questi leggera' in modo 
+continuativo un valore intero da standard input, e lo comunichera' al
+processo padre tramite memoria condivisa. Il processo padre scrivera' ogni
+nuovo valore intero ricevuto su di un file, come sequenza di cifre decimali. 
+I valori scritti su file devono essere separati dal carattere ' ' (blank).
+Il pathname del file di output deve essere comunicato all'applicazione 
+tramite argv[1].
+L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso
+WinAPI) in modo tale che se il processo padre venga colpito il contenuto
+del file di output venga interamente riversato su standard-output.
+Nel caso in cui non vi sia immissione in input, l'applicazione non deve 
+consumare piu' del 5% della capacita' di lavoro della CPU.
+**/
+
 #include<unistd.h>
 #include<stdlib.h>
 #include<stdio.h>
