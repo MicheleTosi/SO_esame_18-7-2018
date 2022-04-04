a08cf6feffce3462010528e9fd5bb944dd78e5bb
Michele Tosi <micheletosi98@gmail.com>
2022-04-04 23:10:21 +0200

    Update 3_7-9-2018.c

--- Esami/3_7-9-2018.c
+++ Esami/3_7-9-2018.c
@@ -1,3 +1,30 @@
+/**
+SPECIFICATION TO BE IMPLEMENTED: 
+Implementare un'applicazione che riceva in input tramite argv[] il 
+nome di un file F ed una stringa indicante un valore numerico N maggiore
+o uguale ad 1.
+L'applicazione, una volta lanciata dovra' creare il file F ed attivare 
+N thread. Inoltre, l'applicazione dovra' anche attivare un processo 
+figlio, in cui vengano attivati altri N thread. 
+I due processi che risulteranno attivi verranno per comodita' identificati
+come A (il padre) e B (il figlio) nella successiva descrizione.
+
+Ciascun thread del processo A leggera' stringhe da standard input. 
+Ogni stringa letta dovra' essere comunicata al corrispettivo thread 
+del processo B tramite memoria condivisa, e questo la scrivera' su una 
+nuova linea del file F. Per semplicita' si assuma che ogni stringa non
+ecceda la taglia di 4KB. 
+
+L'applicazione dovra' gestire il segnale SIGINT (o CTRL_C_EVENT nel caso
+WinAPI) in modo tale che quando il processo A venga colpito esso dovra' 
+inviare la stessa segnalazione verso il processo B. Se invece ad essere 
+colpito e' il processo B, questo dovra' riversare su standard output il 
+contenuto corrente del file F.
+
+Qalora non vi sia immissione di input, l'applicazione dovra' utilizzare 
+non piu' del 5% della capacita' di lavoro della CPU. 
+**/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
